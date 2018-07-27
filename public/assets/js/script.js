2ddbf82dd2f69d6883b19e01245b8c3b150c833d
Dylan Thomas <azelthedemon@gmail.com>
2018-07-26 19:10:23 -0600

    further personalization

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     
-  $(".devour-form").on("submit", function(event) {
+  $(".eatThis").on("submit", function(event) {
     event.preventDefault();
 
     var burger_id = $(this).children(".burger_id").val();
@@ -9,7 +9,7 @@ $(document).ready(function() {
       method: "PUT",
       url: "/burgers/" + burger_id
     }).then(function(data) {
-      // reload page to display devoured burger in proper column
+      
       location.reload();
     });
 
